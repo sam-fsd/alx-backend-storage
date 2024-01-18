eaabde9885f1e12e4746e2b5934f0cc366b4fa9a
sam-fsd <kabirusam037@gmail.com>
2024-01-18 10:24:22 +0000

    enum datatype

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- script that creates table users with id, email, name, country
+-- if table exists, script will not fail
+CREATE TABLE IF NOT EXISTS users
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
